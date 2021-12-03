54fed5d0105844150e3a1338670ac17dbaea198f
Jaytwy24 <jtwy24@gmail.com>
2021-12-03 15:34:03 -0500

    customer files added for authentication ch. 9

--- src/components/employees/EmployeeCard.js
+++ src/components/employees/EmployeeCard.js
@@ -4,9 +4,9 @@ import "./Employee.css"
 export const EmployeeCard = ({employee, location}) => (
     <section className="employee">
         <h3 className="employeeName">{employee.name}</h3>
-        <div className="employeeRate">{employee.hourlyRate}</div>
-        <div className="employeeManager">{employee.manager ? `Manager` : ""}</div>
-        <div className="employeeHours">{employee.fullTime ? `Full Time` : ""}</div>
+        <div className="employeeRate">${employee.hourlyRate}/hr</div>
+        <div className="employeeManager">{employee.manager ? `Manager` : "Kandy Krew"}</div>
+        <div className="employeeHours">{employee.fullTime ? `Full Time` : "Part Time"}</div>
         <div className="employeeFacility">{location.name}</div>
     </section>
 )
